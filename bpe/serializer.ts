5061e6c70563849e130787887b360a37824f0bde
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2026-04-01 13:21:58 +0530

    feat(bpe): 📦 Add BPE model serializer and deserializer
    - Enables persistence of BPE models for reuse

---
+++ bpe/serializer.ts
@@ -0,0 +1,40 @@
+import type { MergeTable } from "./tokenizer";
+
+export interface BpeSerializedModel {
+  type: "bpe";
+  description?: string;
+  version: number;
+  baseVocabSize?: number;
+  mergeCount?: number;
+  trainedVocabSize?: number;
+  mergeTable: MergeTable;
+  notes?: string;
+}
+
+export const serializeBpeModel = (
+  mergeTable: MergeTable,
+): BpeSerializedModel => {
+  const mergeCount = mergeTable.length;
+  const baseVocabSize = 256; // Will figure out how to fetch it
+  const bpeJSON: BpeSerializedModel = {
+    type: "bpe",
+    description: "Byte Pair Encoding Model",
+    version: 1,
+    baseVocabSize: baseVocabSize,
+    mergeCount: mergeCount,
+    trainedVocabSize: baseVocabSize + mergeCount,
+    mergeTable: mergeTable,
+  };
+
+  return bpeJSON;
+};
+
+export const deserializeBpeModel = (
+  bpeJSON: BpeSerializedModel,
+): MergeTable => {
+  if (bpeJSON.type !== "bpe") {
+    throw new Error(`Invalid Model Type: ${bpeJSON.type}`);
+  }
+
+  return bpeJSON.mergeTable;
+};
